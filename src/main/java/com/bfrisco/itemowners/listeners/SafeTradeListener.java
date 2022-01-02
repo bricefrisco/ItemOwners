c9b8fe764ba9f4c52791b3e3097e49b23810f128
Brice <bricemfrisco@gmail.com>
2022-01-01 21:10:16 -0600

    Safetrade repo not found?

--- src/main/java/com/bfrisco/itemowners/listeners/SafeTradeListener.java
+++ src/main/java/com/bfrisco/itemowners/listeners/SafeTradeListener.java
@@ -4,7 +4,7 @@ import com.bfrisco.itemowners.ItemOwners;
 import com.bfrisco.itemowners.constants.LogMessages;
 import com.bfrisco.itemowners.database.ItemEventRepository;
 import com.bfrisco.itemowners.database.ItemEventType;
-import de.oppermann.bastian.safetrade.events.TradeSuccessEvent;
+//import de.oppermann.bastian.safetrade.events.TradeSuccessEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,22 +18,22 @@ public class SafeTradeListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler
-    public void onTradeSuccessEvent(TradeSuccessEvent event) {
-        for (ItemStack item : event.getItemsPlayer1To2()) {
-            String itemId = ItemOwners.getItemId(item);
-            if (itemId == null) continue;
-
-            ItemOwners.getBukkitLogger().info(String.format(LogMessages.TRADED, itemId, event.getPlayer1().getName(), event.getPlayer2().getName()));
-            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> ItemEventRepository.save(ItemEventType.TRADED, itemId, event.getPlayer2()));
-        }
-
-        for (ItemStack item : event.getItemsPlayer2To1()) {
-            String itemId = ItemOwners.getItemId(item);
-            if (itemId == null) continue;
-
-            ItemOwners.getBukkitLogger().info(String.format(LogMessages.TRADED, itemId, event.getPlayer2().getName(), event.getPlayer1().getName()));
-            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> ItemEventRepository.save(ItemEventType.TRADED, itemId, event.getPlayer1()));
-        }
-    }
+//    @EventHandler
+//    public void onTradeSuccessEvent(TradeSuccessEvent event) {
+//        for (ItemStack item : event.getItemsPlayer1To2()) {
+//            String itemId = ItemOwners.getItemId(item);
+//            if (itemId == null) continue;
+//
+//            ItemOwners.getBukkitLogger().info(String.format(LogMessages.TRADED, itemId, event.getPlayer1().getName(), event.getPlayer2().getName()));
+//            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> ItemEventRepository.save(ItemEventType.TRADED, itemId, event.getPlayer2()));
+//        }
+//
+//        for (ItemStack item : event.getItemsPlayer2To1()) {
+//            String itemId = ItemOwners.getItemId(item);
+//            if (itemId == null) continue;
+//
+//            ItemOwners.getBukkitLogger().info(String.format(LogMessages.TRADED, itemId, event.getPlayer2().getName(), event.getPlayer1().getName()));
+//            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> ItemEventRepository.save(ItemEventType.TRADED, itemId, event.getPlayer1()));
+//        }
+//    }
 }
